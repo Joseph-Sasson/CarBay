03cba26863a8b9cff373ec9607a42bdbf99a619c
Joseph <josephsasson1@gmail.com>
2021-08-19 17:03:30 -0400

    add signup and login forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,41 @@
 import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Login from './Login';
-import Signup from './Signup';
+import Login from './component/Login';
+import Signup from './component/Signup';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/">
-          <Login/>
-        </Route>
-      </Switch>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={"/login"}>CarBay</Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/login"}>Sign in</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/signup"}>Sign up</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className="outer">
+          <div className="inner">
+            <Switch>
+              <Route exact path='/' component={Login} />
+              <Route path="/login" component={Login} />
+              <Route path="/signup" component={Signup} />
+            </Switch>
+          </div>
+        </div>
+      </div>
     </Router>
   );
 }
