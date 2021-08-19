2cce58f1bc6b9736f4553697be506f22503ae351
Hadi Abaza <Hadiabaza1@gmail.com>
2021-08-19 17:19:57 -0400

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Login from './component/Login';
-import Signup from './component/Signup';
+import Login from "./component/Login";
+import Signup from "./component/Signup";
 
 function App() {
   return (
@@ -12,14 +12,20 @@ function App() {
       <div className="App">
         <nav className="navbar navbar-expand-lg navbar-light fixed-top">
           <div className="container">
-            <Link className="navbar-brand" to={"/login"}>CarBay</Link>
+            <Link className="navbar-brand" to={"/"}>
+              CarBay
+            </Link>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
-                  <Link className="nav-link" to={"/login"}>Sign in</Link>
+                  <Link className="nav-link" to={"/login"}>
+                    Sign in
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to={"/signup"}>Sign up</Link>
+                  <Link className="nav-link" to={"/signup"}>
+                    Sign up
+                  </Link>
                 </li>
               </ul>
             </div>
@@ -29,7 +35,7 @@ function App() {
         <div className="outer">
           <div className="inner">
             <Switch>
-              <Route exact path='/' component={Login} />
+              <Route exact path="/" component={Login} />
               <Route path="/login" component={Login} />
               <Route path="/signup" component={Signup} />
             </Switch>
