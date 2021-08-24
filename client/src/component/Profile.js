a1ef9fd5ed19dc6c7b80c128bed78a6ae29b88dc
Joseph <josephsasson1@gmail.com>
2021-08-24 15:48:20 -0400

    initial commit

--- client/src/component/Profile.js
+++ client/src/component/Profile.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Profile({user}){
   return (
-    <div>
+    <>
       <div>Name: {user.name}</div>
       <div>Email: {user.email}</div>
       <div>Password: {user.password}</div>
-    </div>
+    </>
   )
 }
 
