2164efa965f8cd0ec6ef30894860e7a1cde894de
Joseph <josephsasson1@gmail.com>
2021-08-26 18:08:18 -0400

    update seeds

--- client/src/component/Car.js
+++ client/src/component/Car.js
@@ -24,8 +24,8 @@ function Car({car, handleBuyNow, setCars, user}){
       <div className="column">
         <div className = "card">
           <img alt="Car" src={car.image} width='300' height='250' />
-          <div>Car name: {car.car_name}</div>
-          <div>Price: {car.price}</div>
+          <div>{car.car_name}</div>
+          <div>Price: ${car.price}</div>
           <div>Owner: {car.user.name}</div>
           <button onClick={()=>handleBuyNow(car)}>Buy now</button>
           <button className = 'change-button' onClick={remove}>Delete car</button>
