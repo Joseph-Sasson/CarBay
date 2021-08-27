ae15c6134744960469e5828162a08ead7b5bea6e
Hadi Abaza <Hadiabaza1@gmail.com>
2021-08-27 13:59:26 -0400

    seed/css

--- client/src/component/Profile.js
+++ client/src/component/Profile.js
@@ -1,81 +1,103 @@
-import React, {useState} from 'react';
-import Car from './Car';
+import React, { useState } from "react";
+import Car from "./Car";
 
-function Profile({user, setUser, cars, setCars, handleBuyNow}){
+function Profile({ user, setUser, cars, setCars, handleBuyNow }) {
   const [errors, setErrors] = useState([]);
   const [userForm, setUserForm] = useState({
     name: user.name,
-    email: user.email
-  })
+    email: user.email,
+  });
 
-  const handleChange = e =>{
-    const name = e.target.name
-    const value = e.target.value
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
     setUserForm({
-      ...userForm, [name]:value
-    })
-  }
+      ...userForm,
+      [name]: value,
+    });
+  };
 
-  const submitChange = (e) =>{
+  const submitChange = (e) => {
     if (window.confirm("Are you sure you want to update this account?"))
-    fetch(`/users/${user.id}`,{
-      method: 'PATCH',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(userForm)
-    }).then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      } else {
-        r.json().then((err) => setErrors(err.errors));
-      }
-    });}
+      fetch(`/users/${user.id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userForm),
+      }).then((r) => {
+        if (r.ok) {
+          r.json().then((user) => setUser(user));
+        } else {
+          r.json().then((err) => setErrors(err.errors));
+        }
+      });
+  };
 
-  const handleDelete = () =>{
+  const handleDelete = () => {
     if (window.confirm("Are you sure you want to delete this account?"))
-    fetch(`/users/${user.id}`,{
-    method: "DELETE",
-      }).then((r)=>{
-        if (r.ok){
-          setUser(null)
-    }})}
+      fetch(`/users/${user.id}`, {
+        method: "DELETE",
+      }).then((r) => {
+        if (r.ok) {
+          setUser(null);
+        }
+      });
+  };
 
   return (
-    <div className = "auth-inners">
-      <h2 id = 'header'>Profile Information</h2>
-      <ul className = 'account-info' >
-        <li className = 'account-list'>Name:
-          <input 
-            type = 'text'
-            name = 'name'
-            value = {userForm.name}
-            onChange = {handleChange}
+    <div className="auth-inners">
+      <h2>My Profile</h2>
+      <ul className="account-info">
+        <li className="account-list">
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={userForm.name}
+            onChange={handleChange}
           />
         </li>
-        <li className = 'account-list'>Email Address:
-        <input 
-            type = 'text'
-            name = 'email'
-            value = {userForm.email}
-            onChange = {handleChange}
+        <li className="account-list">
+          Email Address:
+          <input
+            type="text"
+            name="email"
+            value={userForm.email}
+            onChange={handleChange}
           />
         </li>
       </ul>
-      <button onClick = {handleDelete}>Delete Account</button>
-      <button className = 'change-button' onClick = {submitChange}>Update Account</button>
+      <button className="profile-button" onClick={handleDelete}>
+        Delete Account
+      </button>
+      <button className="profile-button" onClick={submitChange}>
+        Update Account
+      </button>
       <div>
         {errors.map((err) => (
           <span>!{err}</span>
         ))}
       </div>
-      <h2 id = 'header'>My cars</h2>
-      <div className="row">
-      {cars.filter(car=>car.user.id === user.id).map(car=> {return <Car key={car.id} car={car} handleBuyNow={handleBuyNow} setCars={setCars} user={user} />})}
+      <h2>My cars</h2>
+      <div className="profile-car">
+        {cars
+          .filter((car) => car.user.id === user.id)
+          .map((car) => {
+            return (
+              <Car
+                key={car.id}
+                car={car}
+                handleBuyNow={handleBuyNow}
+                setCars={setCars}
+                user={user}
+              />
+            );
+          })}
         {/* {cars.map(car=>{
           if (user.id === car.user.id) {return <Car key={car.id} car={car} setCars={setCars} handleBuyNow={handleBuyNow} user={user} />}
         })} */}
       </div>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
