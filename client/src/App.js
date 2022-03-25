9d23474672db36aedf0685cb99268d8dac52ab76
Joseph <josephsasson1@gmail.com>
2022-03-25 13:27:16 -0400

    fix search bar to match other inputs in the car form

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import "./App.css";
 import Login from "./component/Login";
 import Signup from "./component/Signup";
 import Home from "./component/Home";
